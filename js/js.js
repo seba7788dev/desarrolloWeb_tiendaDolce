441bf4a7c4c0f5a1da72a2a113598838e8636d35
seba7788dev <cardenas.sebastian.claudio@gmail.com>
2021-02-09 20:40:53 -0300

    terminado, agregado JS

---
+++ js/js.js
@@ -0,0 +1,28 @@
+let ubicacionPrincipal=window.scrollY;/* me indica en q parte de la pagina estoy situado */
+
+window.addEventListener("scroll", function(){
+    let desplazamientoActual=window.scrollY;
+    if(ubicacionPrincipal>=desplazamientoActual){
+        document.getElementsByTagName("nav")[0].style.top="0px"
+    }else{
+        document.getElementsByTagName("nav")[0].style.top="-100px"
+    }
+
+    ubicacionPrincipal=desplazamientoActual
+})
+
+/*  == Menu == */
+
+let enlacesHeader=document.querySelectorAll(".enlaces-header")[0];
+let semaforo=true;
+
+document.querySelectorAll(".hamburguer")[0].addEventListener("click",function(){
+    if(semaforo){
+        document.querySelectorAll(".hamburguer")[0].style.color="#fff";
+        semaforo=false
+    }else{ document.querySelectorAll(".hamburguer")[0].style.color="#000";
+    semaforo=true
+
+    }
+    enlacesHeader.classList.toggle("menudos");
+})
